0f0adc366f031ea1a8ff9db667586462f4aaeae4
Mato <martinmojzis06@gmail.com>
2022-05-17 00:35:09 +0200

    cms cleanup

--- cms/config/app.php
+++ cms/config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'debug' => true,
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
@@ -137,7 +137,7 @@ return [
     |
     */
 
-    'key' => 'base64:oxSDgj9Up7Pa3hGsiRFyzy36YLtcbRnVLyFngIWUh+Y=',
+    'key' => env('APP_KEY', ''),
 
     'cipher' => 'AES-256-CBC',
 
