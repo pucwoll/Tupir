ad8cb2c89a6986dc879b2b416a74574da7ca3b63
Jakub Jelínek <jakubkoje@gmail.com>
2022-07-12 23:26:23 +0200

    colors

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -4,7 +4,6 @@
       <ion-toolbar>
         <ion-title
           color="light"
-          class="title"
         >
           Profile
         </ion-title>
@@ -13,7 +12,6 @@
             <ion-icon
               slot="icon-only"
               :icon="cog"
-              class="settings"
             />
           </ion-button>
         </ion-buttons>
@@ -28,20 +26,4 @@ import { cog } from 'ionicons/icons'
 </script>
 
 <style scoped>
-ion-toolbar {
-	--background: transparent;
-	--border-width: 0 !important;
-}
-
-ion-content {
-	--background: linear-gradient(163.53deg, rgba(189, 224, 254, 0.5) 26.71%, rgba(251, 214, 210, 0) 88.59%), linear-gradient(137.84deg, rgba(255, 175, 204, 0.3) 0%, rgba(241, 144, 183, 0) 81.9%), linear-gradient(338.12deg, rgba(255, 200, 221, 0.8) 14.29%, rgba(206, 73, 191, 0) 90.05%), linear-gradient(106.23deg, rgba(205, 180, 219, 0.7) -1.99%, rgba(166, 62, 197, 0) -1.99%);
-}
-
-.title {
-	text-shadow: 1px 1px rgba(var(--ion-color-dark-rgb), 0.5);
-}
-
-.settings {
-	filter: drop-shadow(1px 1px rgba(0, 0, 0, .2));
-}
 </style>
