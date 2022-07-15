73272ec8de95d669c3cf76c25a845b9d41cc1538
matomos <martinmojzis06@gmail.com>
2022-07-15 02:19:35 +0200

    .

--- cms/plugins/libuser/delete/routers.php
+++ cms/plugins/libuser/delete/routers.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LibUser\UserApi\Http\Middlewares\Check;
 use LibUser\UserApi\Http\Middlewares\Authenticate;
 use LibUser\Delete\Http\Controllers\DeleteController;
+use WApi\ApiException\Http\Middlewares\ApiExceptionMiddleware;
 
 Route::group([
     'prefix' => 'api/v1/auth',
-    'middleware' => Authenticate::class,
+    'middleware' => [
+        ApiExceptionMiddleware::class,
+        'api',
+        Check::class,
+        Authenticate::class,
+    ],
 ], function (Route $route) {
     $route
         ->delete('/delete', [DeleteController::class, 'handle']);
