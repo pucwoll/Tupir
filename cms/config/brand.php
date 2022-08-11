1465978160b49d649423c63352cb5f2b803ecbe2
matomos <martinmojzis06@gmail.com>
2022-08-11 15:29:01 +0200

    setup brand & composer.json clenaup

---
+++ cms/config/brand.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Specify the default backend brand settings
+    |--------------------------------------------------------------------------
+    |
+    | These parameter values can be overridden by the backend Brand settings
+    |
+    */
+
+    'logoPath' => '$/apptupir/brand/assets/images/logo.png',
+    'faviconPath' => '$/apptupir/brand/assets/images/logo.png',
+    'appName' => 'Tupir Server',
+    'tagline' => 'Complete administration area for Tupir App.',
+    'primaryColor' => "#292d3e",
+    'secondaryColor' => "#facadc",
+    'accentColor' => '#bfe0fc',
+    'menuMode' => 'inline', // options: inline, inline_no_icons, tile, collapse
+    'customLessPath' => null
+];
---
+++ cms/plugins/apptupir/brand/Plugin.php
@@ -0,0 +1,45 @@
+<?php namespace AppTupir\Brand;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Brand Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Brand',
+            'description' => 'No description provided yet...',
+            'author'      => 'AppTupir',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+}
